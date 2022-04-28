50db2c480598effbc94164fa294345f0dbcb6c72
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-04-28 01:27:01 -0500

    requiring modules in practice

---
+++ test-module-1.js
@@ -0,0 +1,28 @@
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multiply(a, b) {
+//     return a * b;
+//   }
+
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+
+//used when we want to export one single value
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+};
